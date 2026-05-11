e5cfe2be797466e916ea17862c5da2eefad6118a
Reinhold-Jesse <88349887+Reinhold-Jesse@users.noreply.github.com>
2026-05-11 16:43:32 +0200

    new build css & js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,23 @@
-//
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.data('reveal', () => ({
+    shown: false,
+
+    init() {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    this.shown = true;
+                    observer.disconnect();
+                }
+            },
+            { rootMargin: '0px 0px -12% 0px', threshold: 0.16 },
+        );
+
+        observer.observe(this.$el);
+    },
+}));
+
+Alpine.start();
